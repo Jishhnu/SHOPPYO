9236fab1e13bf7686de35fa9e829fb2ad4ffd62c
jishnu <jishnugopikrishnan0@gmail.com>
2026-04-07 12:58:37 +0530

    email verifications

--- Page/settings.py
+++ Page/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
 ]
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 AUTH_USER_MODEL = 'core.User'
 
@@ -177,3 +179,12 @@ load_dotenv()
 
 RAZORPAY_KEY="rzp_test_SZuaJynYnVcowM"
 RAZORPAY_SECRET="F11K2ZGKzQwda8ACP1yz4ygZ"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'shoppyo.online@gmail.com'
+EMAIL_HOST_PASSWORD = 'wjofubkdcljogzsg'
+DEFAULT_FROM_EMAIL = 'shoppyo.online@gmail.com'
